53a960deeaa51dad692ae997fdf22ae59eab5e26
Brooke Fisher <brookenfish1@gmail.com>
2019-04-19 19:11:09 -0400

    Updates

---
+++ precache-manifest.84d7a6e5349b98912e1aea556bc49b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecc4c4034f18fd3c8e1b",
+    "url": "/progressive-project/static/js/runtime~main.d9bcf800.js"
+  },
+  {
+    "revision": "81c80f1ee6d3f6316d7e",
+    "url": "/progressive-project/static/js/main.87ad0986.chunk.js"
+  },
+  {
+    "revision": "8e05c86a285a4ffbefde",
+    "url": "/progressive-project/static/js/2.fd5e7c71.chunk.js"
+  },
+  {
+    "revision": "81c80f1ee6d3f6316d7e",
+    "url": "/progressive-project/static/css/main.38c13aa9.chunk.css"
+  },
+  {
+    "revision": "8e05c86a285a4ffbefde",
+    "url": "/progressive-project/static/css/2.e70ec534.chunk.css"
+  },
+  {
+    "revision": "521f8d8a1e90d82f35080f279d88ed57",
+    "url": "/progressive-project/index.html"
+  }
+];
